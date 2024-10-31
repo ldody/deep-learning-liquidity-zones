00e316f344c6fe046d3458fbaf2b99e008bf829f
ldody <leo.dody01@gmail.com>
2024-10-31 11:56:49 +0100

    Update FOB_prepro.bash

--- slurm/FOB_prepro.bash
+++ slurm/FOB_prepro.bash
@@ -5,7 +5,7 @@
 #SBATCH --mem-per-cpu=2000
 #SBATCH --mail-type=all
 #SBATCH --mail-user=leo.dody1@univ-lyon3.fr
-#SBATCH --output=FOB_prepro_%A_%a.out
+#SBATCH --output=FOB_prepro_%a.out
 #SBATCH --job-name=FOB_prepro_%A_%a
 #SBATCH --partition=c6420-ib100
 #SBATCH --array=0-20%10
@@ -16,6 +16,6 @@ module use /easybuild/AlmaLinux/8/skylake-avx512/mlxln5.5/foss2022b/modules/all
 module load Python/3.10.4-GCCcore-12.2.0
 source /home_nfs/polytech/leo.dody/PhD/Article_2/PhD_article_2/.venv/bin/activate
 
-python3 ../src/data/prepro_FOB.py -sa True
+python3 ../src/data/prepro_FOB.py -sa True --job_id $SLURM_ARRAY_TASK_ID
 
 deactivate
