129f0324b7c2eb165970fe134e5f9cd94fc695c4
ldody <leo.dody01@gmail.com>
2024-11-04 13:41:26 +0100

    requirements

--- src/data/prepro_FOB.py
+++ src/data/prepro_FOB.py
@@ -4,6 +4,7 @@ import zipfile
 import pandas as pd
 import numpy as np
 import argparse
+from fastparquet import write
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from FOBDBM import FOBDataBaseManagement as fobdm
