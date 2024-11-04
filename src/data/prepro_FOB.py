d05fb9080b31df02e3b5a65c0e997590165c5d7a
ldody <leo.dody01@gmail.com>
2024-11-04 16:59:38 +0100

    debug

--- src/data/prepro_FOB.py
+++ src/data/prepro_FOB.py
@@ -187,7 +187,7 @@ class FOBPreprocessor:
 						self.LOB.loc[self.LOB[cond].index, 'size'] += row['order_size']
 						
 				if len(self.LOB[(self.LOB['price'] == row['order_price']) & (self.LOB['side'] == row['order_side'])]) == 1:
-					if self.LOB.loc[(self.LOB['price'] == row['order_price']) & (self.LOB['side'] == row['order_side']), 'size'] < 0:
+					if self.LOB.loc[(self.LOB['price'] == row['order_price']) & (self.LOB['side'] == row['order_side']), 'size'][0] < 0:
 						print(row, self.LOB[(self.LOB['price'] == row['order_price']) & (self.LOB['side'] == row['order_side'])], self.isin, self.date)
 
 			self.LOB = self.LOB.drop(self.LOB[(self.LOB == 0).any(axis=1)].index)    
