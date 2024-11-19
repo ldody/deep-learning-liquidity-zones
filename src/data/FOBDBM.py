55446e34641d7108d1a6e1ee297ff0d396f12405
ldody <leo.dody01@gmail.com>
2024-11-19 20:47:53 +0100

    Update FOBDBM.py

--- src/data/FOBDBM.py
+++ src/data/FOBDBM.py
@@ -217,7 +217,7 @@ class FOBDataBaseManagement():
 			self.DB = pd.read_csv(os.path.join(self.path, self.DB_file), index_col=0)
 			
 			self.DB['allocate'] = np.nan
-			print(self.DB[self.DB['allocate'] != np.nan)
+			print(self.DB[self.DB['allocate'] != np.nan])
 			
 			self.DB.to_csv(os.path.join(self.path, self.DB_file), header=True)
 			
