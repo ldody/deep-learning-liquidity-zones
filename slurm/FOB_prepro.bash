252f7431654d81176d42e768ab71978e0b59a66a
ldody <leo.dody01@gmail.com>
2024-11-19 20:54:34 +0100

    Update FOB_prepro.bash

--- slurm/FOB_prepro.bash
+++ slurm/FOB_prepro.bash
@@ -5,10 +5,11 @@
 #SBATCH --mem-per-cpu=2000
 #SBATCH --mail-type=all
 #SBATCH --mail-user=leo.dody1@univ-lyon3.fr
-#SBATCH --output=/dev/null
+#SBATCH --output=FOB_prepro_%a.out
+#####/dev/null
 #SBATCH --job-name=FOB_prepro_%A_%a
 #SBATCH --partition=c6420-ib100
-#SBATCH --array=0-857%40
+#SBATCH --array=0-41%40
 
 
 module purge
