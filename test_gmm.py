10284bca9d935a4846c8d17dc0cf0a67d2bc8247
ldody <leo.dody01@gmail.com>
2024-11-20 18:30:53 +0100

    check

--- test_gmm.py
+++ test_gmm.py
@@ -65,4 +65,5 @@ plt.savefig('fig_gmm.png')
 
 df_res = pd.DataFrame(data_with_clusters, columns=['price','smoothed_size','side','cluster'])
 df_res['size'] = df_lob['size']
+print(df_lob['size'])
 df_res.to_csv('results_GMM.csv')
