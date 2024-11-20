04abc1ff18778ddeba49e14896b7413bcd6f8b6d
ldody <leo.dody01@gmail.com>
2024-11-20 14:10:23 +0100

    Update test_gmm.py

--- test_gmm.py
+++ test_gmm.py
@@ -2,5 +2,5 @@ import numpy as np
 from sklearn.mixture import GaussianMixture
 X = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
 gm = GaussianMixture(n_components=2, random_state=0).fit(X)
-gm.means_
-gm.predict([[0, 0], [12, 3]])
+print(gm.means_)
+print(gm.predict([[0, 0], [12, 3]]))
