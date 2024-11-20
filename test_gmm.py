7e99146d4ab67edbe5958b2390f48bde77346076
ldody <leo.dody01@gmail.com>
2024-11-20 15:02:23 +0100

    Update test_gmm.py

--- test_gmm.py
+++ test_gmm.py
@@ -3,30 +3,31 @@ import matplotlib.pyplot as plt
 from sklearn.mixture import GaussianMixture
 from sklearn.preprocessing import StandardScaler
 
-# Génération de données simulées
-np.random.seed(42)
+path = os.path.dirname(os.path.abspath(__file__))
+root_path = path
+while os.path.basename(root_path) != 'PhD_article_2':
+	root_path =  os.path.dirname(root_path)
+	
+processed_path = os.path.join(self.root_path,'data','processed','FOB')
+processed_path_LOB = os.path.join(self.processed_path,'LOB')
 
-# Simuler des prix et des volumes (zones de liquidité denses et bruit)
-prices = np.concatenate([
-	np.random.normal(100, 1, 500),  # Cluster autour de 100
-	np.random.normal(105, 1, 300),  # Cluster autour de 105
-	np.random.uniform(90, 110, 200) # Bruit uniforme
-])
-volumes = np.concatenate([
-	np.random.randint(1000, 10000, 500),  # Volumes élevés autour de 100
-	np.random.randint(5000, 20000, 300), # Volumes élevés autour de 105
-	np.random.randint(0, 200, 200)       # Bruit de faible volume
-])
 
-# Construire le tableau des données
-data = np.array([prices, volumes]).T
+df_lob = pd.read_parquet(os.path.join(processed_path_LOB, 'NL0000226223_final_LOB.parquet.gzip')).reset_index().drop_duplicates()
 
-# Standardiser les données (important pour GMM)
-scaler = StandardScaler()
+df_lob = df_lob[df_lob['index'] == '2023-10-31 15:01:00']
+df_lob['side'] = df_lob['side'].apply(lambda x: 1 if x == 'Buy' else -1)
+
+display(df_lob[['price','size','side']].to_numpy())
+
+data = df_lob[['price','size','side']].to_numpy()
+
+scaler = MinMaxScaler(feature_range=(-1, 1))
 data_scaled = scaler.fit_transform(data)
+data_scaled[:,2] = data[:,2]
+display(data_scaled)
 
 # Appliquer GMM
-n_components = 3  # Définir le nombre de clusters supposés
+n_components = 10  # Définir le nombre de clusters supposés
 gmm = GaussianMixture(n_components=n_components, covariance_type='full', random_state=42)
 clusters = gmm.fit_predict(data_scaled)
 
