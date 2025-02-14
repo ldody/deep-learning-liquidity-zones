6326354b730de5a85802e6dd858500916e964fef
ldody <leo.dody01@gmail.com>
2025-02-14 17:06:55 +0100

    Update clustering_analysis_main.py

--- src/analysis/clustering_analysis_main.py
+++ src/analysis/clustering_analysis_main.py
@@ -95,7 +95,7 @@ class clustering_analysis(Base):
 		
 		self.df_data = pd.read_parquet(self.to_process['path'])
 		self.df_data = self.df_data.set_index('index').between_time('9:00', '17:00').reset_index()
-        self.df_data = self.df_data[self.df_data['err'] == ]
+		self.df_data = self.df_data[self.df_data['err'].isin([0,1])]
 		
 		
 	def array_process(self):
