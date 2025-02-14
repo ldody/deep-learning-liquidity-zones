e5e467193dd580feb68f2cbcb29ab2c6841660e9
ldody <leo.dody01@gmail.com>
2025-02-14 17:14:13 +0100

    Update clustering_analysis_main.py

--- src/analysis/clustering_analysis_main.py
+++ src/analysis/clustering_analysis_main.py
@@ -94,6 +94,7 @@ class clustering_analysis(Base):
 		self.df_ohlcv = self.df_ohlcv.set_index('Local Time').between_time('9:00', '17:30').reset_index()
 		
 		self.df_data = pd.read_parquet(self.to_process['path'])
+		print(self.to_process['path'])
 		self.df_data = self.df_data.set_index('index').between_time('9:00', '17:00').reset_index()
 		self.df_data = self.df_data[self.df_data['err'].isin([0,1])]
 		
