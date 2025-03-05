6ced9c6c3ade3e71b72fe6164d0b50eacc27b450
ldody <leo.dody01@gmail.com>
2025-03-05 15:31:45 +0100

    Update data_cleaning.bash

--- pour_tom/data_cleaning.bash
+++ pour_tom/data_cleaning.bash
@@ -8,7 +8,6 @@
 #SBATCH --mail-user=leo.dody1@univ-lyon3.fr
 #SBATCH --output=data_cleaning.out #/dev/null
 #SBATCH --job-name=data_cleaning
-#SBATCH --partition=c6420-ib100
 #SBATCH --nodelist=c6420-ib100-005
 
 
