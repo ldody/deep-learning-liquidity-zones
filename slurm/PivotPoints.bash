838f98cc778db91a01f26737de65e7d775864c15
ldody <leo.dody01@gmail.com>
2025-03-18 15:40:56 +0100

    Create PivotPoints.bash

---
+++ slurm/PivotPoints.bash
@@ -0,0 +1,23 @@
+#!/bin/sh
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem-per-cpu=4000
+#SBATCH --mail-type=all
+#SBATCH --mail-user=leo.dody1@univ-lyon3.fr
+#SBATCH --output=PP_model.out #/dev/null
+#SBATCH --job-name=PP_model
+#SBATCH --partition=c6420-ib100
+
+
+module purge
+module use /easybuild/AlmaLinux/8/skylake-avx512/mlxln5.5/foss2022b/modules/all
+module load Python/3.10.4-GCCcore-12.2.0
+source /home_nfs/polytech/leo.dody/PhD/Article_2/PhD_article_2/.venv/bin/activate
+
+export PYTHONUNBUFFERED=TRUE
+
+python3 ../src/comp_models/pivot_points.py -sa True 
+
+deactivate
