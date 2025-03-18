4e2b458b1c88572803e44778c16e29dfa1ac4303
ldody <leo.dody01@gmail.com>
2025-03-18 16:48:14 +0100

    Update pivot_points.py

--- src/comp_models/pivot_points.py
+++ src/comp_models/pivot_points.py
@@ -132,7 +132,7 @@ class PivotPoints():
 				r = min(7, len(tmp))
 
 				test.loc[test['Local Time'] == row['Local Time'], 'Accuracy'] = a/r if r != 0 else np.nan
-				test.loc[test['Local Time'] == row['Local Time'], 'MAE'] = d/a if a != 0 else np.nan
+				test.loc[test['Local Time'] == row['Local Time'], 'MAE'] = (d/a)/self.to_process['Tick_step'] if a != 0 else np.nan
 				
 			return test
 		
