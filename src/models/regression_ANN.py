0b1fe5138813e0e372581265ba57ca83dacce39d
ldody <leo.dody01@gmail.com>
2025-04-03 14:17:59 +0200

    Update regression_ANN.py

--- src/models/regression_ANN.py
+++ src/models/regression_ANN.py
@@ -102,6 +102,7 @@ class ANN_model():
 
 		# === 4. Model Input ===
 		input_gru = Input(shape=input_shape, name='OHLCV')
+		print(len(input_gru))
 		input_cnn = Reshape((input_shape[0], input_shape[1], 1))(input_gru)  # Format (100,5,1)
 
 		# === 5. Feature Extraction ===
