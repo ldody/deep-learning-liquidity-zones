9725e1ef00cda144cf1536884cbce793a1599e5e
ldody <leo.dody01@gmail.com>
2025-08-01 13:53:57 +0200

    Update classification.py

--- src/comp_models/classification.py
+++ src/comp_models/classification.py
@@ -16,6 +16,8 @@ warnings.simplefilter(action='ignore', category=Warning)
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'models'))
+print(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'models'))
+print(os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'models')))
 from regression_preprocessing import RegressionPreprocess as rprepro
 
 
