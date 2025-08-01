6353fd13458738eb2aa8acb1e3ccd4bef41f3b00
ldody <leo.dody01@gmail.com>
2025-08-01 13:53:01 +0200

    Update classification.py

--- src/comp_models/classification.py
+++ src/comp_models/classification.py
@@ -15,7 +15,7 @@ from sklearn.multioutput import MultiOutputClassifier
 warnings.simplefilter(action='ignore', category=Warning)
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'models'))
 from regression_preprocessing import RegressionPreprocess as rprepro
 
 
