5b56d7d5a3ec457933f379a452fed4efbed41b72
ldody <leo.dody01@gmail.com>
2025-12-09 16:23:49 +0100

    Update backtest.py

--- src/backtest/backtest.py
+++ src/backtest/backtest.py
@@ -98,6 +98,7 @@ class ohlcv_bid_ask(Base):
 		self.df_data = self.df_data.reset_index(drop=True)
 		self.df_data = self.df_data.groupby(['index','side']).last()
 		self.df_data = self.df_data.unstack(level=1)
+		print(self.df_data)
 		self.df_data.columns = [f"{lvl0}_{lvl1}" for (lvl0, lvl1) in self.df_data.columns]
 		self.df_data = self.df_data.rename(columns={
 			"price_Buy": "bid",
