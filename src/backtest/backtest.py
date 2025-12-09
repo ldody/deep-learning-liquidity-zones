d330acb28727e4458ef4f0864ca3f940013581e8
ldody <leo.dody01@gmail.com>
2025-12-09 16:35:24 +0100

    Update backtest.py

--- src/backtest/backtest.py
+++ src/backtest/backtest.py
@@ -80,7 +80,7 @@ class ohlcv_bid_ask(Base):
 		"""
 		self.df_ohlcv = pd.read_csv(self.to_process['OHLCV'])
 		self.df_ohlcv['Local Time'] = pd.to_datetime(self.df_ohlcv['Local Time'])
-		self.df_ohlcv = self.df_ohlcv.set_index('Local Time').between_time('9:00', '17:00').reset_index()
+		self.df_ohlcv = self.df_ohlcv.set_index('Local Time').between_time('9:00', '17:00')
 		#self.df_ohlcv = self.df_ohlcv['Volume' not in self.df_ohlcv.columns]
 		
 		self.df_data = pd.read_parquet(self.to_process['path'])
